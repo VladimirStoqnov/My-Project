f7d9b0af13000c3876790046634352c4b65f8b08
VladimirStoqnov <vladimirstoqnov7@gmail.com>
2022-12-13 19:32:12 +0200

    Update

--- cosplay_project/settings.py
+++ cosplay_project/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = 'django-insecure-7-o%=tqxsqr3b82u&^jtt!40@4kxp%yr!own$1pg915#i8atl)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 # Application definition
 
