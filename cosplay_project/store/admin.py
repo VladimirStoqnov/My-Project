826164ecafc0dd38711d69b64279cea118bdbdc9
VladimirStoqnov <vladimirstoqnov7@gmail.com>
2022-12-15 13:23:27 +0200

    Adding Store App

--- cosplay_project/store/admin.py
+++ cosplay_project/store/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from cosplay_project.store.models import Items
 
-@admin.register(admin.ModelAdmin)
-class StoreAdmin(Items):
+
+@admin.register(Items)
+class StoreAdmin(admin.ModelAdmin):
     pass
